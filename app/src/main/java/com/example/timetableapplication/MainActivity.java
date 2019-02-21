02b243f84d7082e3e45d3804af18557ce8114695
AbdulaleemSaleh1 <33027355+AbdulaleemSaleh1@users.noreply.github.com>
2019-02-21 15:59:56 +0000

    Created pages and login functionality

--- app/src/main/java/com/example/timetableapplication/MainActivity.java
+++ app/src/main/java/com/example/timetableapplication/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.timetableapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.UsernameText);
+        Password = (EditText)findViewById(R.id.PasswordText);
+        Login = (Button)findViewById(R.id.LoginButton);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword) {
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))) {
+            Intent intent = new Intent(MainActivity.this, TimeTablePage.class);
+            startActivity(intent);
+        }
     }
 }
