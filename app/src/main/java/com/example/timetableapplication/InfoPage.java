3859b6dd35cdcae96b799b8c05278bb194e0efb1
AbdulaleemSaleh1 <33027355+AbdulaleemSaleh1@users.noreply.github.com>
2019-02-25 12:37:34 +0000

    Added Navigation to InfoPage

--- app/src/main/java/com/example/timetableapplication/InfoPage.java
+++ app/src/main/java/com/example/timetableapplication/InfoPage.java
@@ -1,7 +1,9 @@
 package com.example.timetableapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class InfoPage extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class InfoPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
+    }
+    public void MapBtnClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),   MapPage.class);
+        startActivity(myIntent);
+    }
+    public void HomeBtnClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),   TimeTablePage.class);
+        startActivity(myIntent);
+    }
+    public void InfoBtnClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),  InfoPage.class);
+        startActivity(myIntent);
     }
 }
