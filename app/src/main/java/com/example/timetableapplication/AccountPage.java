b99c4f36e528e4c970cbc7f610cbe37fd17f9f8e
AbdulaleemSaleh1 <33027355+AbdulaleemSaleh1@users.noreply.github.com>
2019-03-11 14:04:36 +0000

    Created background Animation Only implemented on Account so as to test it

--- app/src/main/java/com/example/timetableapplication/AccountPage.java
+++ app/src/main/java/com/example/timetableapplication/AccountPage.java
@@ -1,16 +1,28 @@
 package com.example.timetableapplication;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class AccountPage extends AppCompatActivity {
 
+    ConstraintLayout myLayout;
+    AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
+
+        myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
+
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
     }
     public void MapPG(View v){
         Intent myIntent = new Intent(getBaseContext(),   MapPage.class);
