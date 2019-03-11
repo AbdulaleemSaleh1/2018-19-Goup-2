87d3f5d0c6c41e192947e860a9d0e2a88cbeb6c9
AbdulaleemSaleh1 <33027355+AbdulaleemSaleh1@users.noreply.github.com>
2019-03-11 13:00:17 +0000

    Created and completed AccountPage Added Navigation for AccountPage

---
+++ app/src/main/java/com/example/timetableapplication/AccountPage.java
@@ -0,0 +1,27 @@
+package com.example.timetableapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AccountPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_page);
+    }
+    public void MapPG(View v){
+        Intent myIntent = new Intent(getBaseContext(),   MapPage.class);
+        startActivity(myIntent);
+    }
+    public void HomePG(View v){
+        Intent myIntent = new Intent(getBaseContext(),   TimeTablePage.class);
+        startActivity(myIntent);
+    }
+    public void InfoPG(View v){
+        Intent myIntent = new Intent(getBaseContext(),  InfoPage.class);
+        startActivity(myIntent);
+    }
+}
