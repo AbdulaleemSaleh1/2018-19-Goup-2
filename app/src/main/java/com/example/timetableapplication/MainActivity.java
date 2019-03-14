e1d40f2fa8906cc2e9727833f316023600b32cb9
muratgoksen96 <32961103+muratgoksen96@users.noreply.github.com>
2019-03-14 17:14:48 +0000

    Google Maps API on Map Page

--- app/src/main/java/com/example/timetableapplication/MainActivity.java
+++ app/src/main/java/com/example/timetableapplication/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.timetableapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     private EditText Name;
     private EditText Password;
@@ -18,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         Name = (EditText)findViewById(R.id.UsernameText);
         Password = (EditText)findViewById(R.id.PasswordText);
@@ -36,5 +50,13 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, TimeTablePage.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng Brunel = new LatLng(51.53252706501752,-0.48024521922898716);
+        map.addMarker(new MarkerOptions().getPosition(Brunel).title("Brunel"));
+    map.moveCamera(CameraUpdateFactory.newLatLng(Brunel));
     }
 }
