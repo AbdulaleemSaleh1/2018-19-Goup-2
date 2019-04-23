459c4807ac9724d06d0322e855546789e48a8344
AnkurP13 <33151447+1619450@users.noreply.github.com>
2019-04-23 02:02:29 +0100

    Added campus and logo image

--- app/src/main/java/com/example/timetableapplication/MapPage.java
+++ app/src/main/java/com/example/timetableapplication/MapPage.java
@@ -3,14 +3,21 @@ package com.example.timetableapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MapPage extends AppCompatActivity {
-
+    private static final String TAG = "MapPage";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_page);
+
+        Log.d(TAG, "OnCreate: started.");
+        ImageView Secondimage = (ImageView)findViewById(R.id.Secondimage);
+        int imageResource = getResources().getIdentifier("@drawable/brunelcampus", null, this.getPackageName());
+        Secondimage.setImageResource(imageResource);
     }
     public void HomePG(View v){
         Intent myIntent = new Intent(getBaseContext(),   TimeTablePage.class);
